6db564975367954c6af63026fa7279709a1ca7e8
Bipin Maurya <96836433+bipinmaurya0@users.noreply.github.com>
2022-01-30 02:21:45 -0800

    Add files via upload

---
+++ 30 th Jan  Weekly Test/test1.js
@@ -0,0 +1,18 @@
+
+var num=prompt("enter the number");
+num=parseInt(num);
+if(isNaN(num))
+{
+alert("please enter valid number");
+}
+else if(num==2 || num==3)
+{
+console.log(num+"is a prime number");
+}
+else if(num%2==0 ||num%3== 0)
+{
+console.log(num  + " is not a prime number ");
+}
+else{
+console.log(num  + " is a prime number ");
+}
